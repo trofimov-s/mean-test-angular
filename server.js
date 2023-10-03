c6a5c8f88967bdb058110e84483e129f00715ec2
Serhii Trofimov <trofim.serhii@gmail.com>
2023-10-03 15:36:27 +0300

    fix path

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ const path = require("path");
 const app = express();
 app.use(express.static(__dirname + "/dist/mean-test"));
 app.get("/*", function (req, res) {
-  res.sendFile(path.join(__dirname + "/dist/mean-test/index.html"));
+  res.sendFile(path.join(__dirname + "dist", "mean-test", "index.html"));
 });
 app.listen(process.env.PORT || 8080);
