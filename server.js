a17797c8bca791ec9a56c2f64dc270d8d7db6252
Serhii Trofimov <trofim.serhii@gmail.com>
2023-10-03 14:54:23 +0300

    add

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+app.use(express.static(__dirname + "/dist/mean-test"));
+app.get("/*", function (req, res) {
+  res.sendFile(path.join(__dirname + "/dist/mean-test/index.html"));
+});
+app.listen(process.env.PORT || 8080);
